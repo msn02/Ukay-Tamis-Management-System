e1d57e698cb12d8c6977cd32cd99ee10ff826814
Misty Shaine Niones <125946850+msn02@users.noreply.github.com>
2024-05-14 14:57:32 +0800

    style: modified buttons, added dropdown

--- pages/orders.php
+++ pages/orders.php
@@ -17,10 +17,10 @@
             <!-- options -->
             <form class="" action="">
                 <input type="radio" class="btn-check rounded-1" name="record_option" id="items" autocomplete="off" checked>
-                <label class="btn btn-dark px-3 py-2" for="items">SINGLE ITEMS</label>
+                <label class="btn btn-outline-dark px-3 py-2" for="items">SINGLE ITEMS</label>
 
                 <input type="radio" class="btn-check rounded-1" name="record_option" id="box" autocomplete="off">
-                <label class="btn btn-dark px-3 py-2" for="box">BOXES</label>
+                <label class="btn btn-outline-dark px-3 py-2" for="box">BOXES</label>
 
                 <!-- <button class="px-4 py-2 btn btn-secondary border rounded-1" onclick="">SUBSCRIPTION</button> -->
             </form>
@@ -33,28 +33,45 @@
                     <tr class="table_head">
                         <th scope="col">Transaction ID</th>
                         <th scope="col">User ID</th>
-                        <th scope="col">Product ID</th>
-                        <th scope="col">Product Name</th>
+                        <th scope="col">Item ID</th>
+                        <th scope="col">Item Name</th>
                         <th scope="col">Quantity</th>
                         <th scope="col">Shipping Fee</th>
                         <th scope="col">Total Price</th>
                         <th scope="col">Payment Method</th>
-                        <th scope="col">Status</th>
+                        <th scope="col-1">Status</th>
                         <th scope="col">Actions</th>
                     </tr>
                 </thead>
                 <tbody>
-                    <tr class="table_body">
+                    <tr class="table_body align-middle">
                         <td scope="row" class="text-start">1234</td>
                         <td class="text-start">1234</td>
                         <td class="text-center">1234</td>
                         <td class="text-center">Summer Fest</td>
-                        <td class="text-end">1</td>
+                        <td class="text-center">1</td>
                         <td class="text-end">PHP 100</td>
                         <td class="text-end fw-bold">PHP 600</td>
                         <td class="text-center">GCash</td>
-                        <td class="text-center">Dropdown</td>
-                        <td class="text-center">GCash</td>
+                        <!-- status -->
+                        <td class="text-center hstack gap-2 d-flex justify-content-center">
+                            <form class="form_option">
+                                <select id="status" class="form-select rounded-1" disabled>
+                                    <option selected>Pending</option>
+                                    <option value="1">Packed</option>
+                                    <option value="2">Shipped</option>
+                                </select>
+                            </form>
+                        </td>
+                        <!-- actions -->
+                        <td class="text-center">
+                            <div class="m-0 p-0 hstack gap-2 d-flex justify-content-center gray_btn">
+                                <!-- edit order status -->
+                                <button id="edit_button" class="btn btn-secondary rounded-1"><i class="bi bi-pencil-square"></i></button>
+                                <!-- delete order -->
+                                <button class="btn btn-danger rounded-1"><i class="bi bi-trash3-fill"></i></button>
+                            </div>
+                        </td>
                     </tr>
                 </tbody>
             </table>
