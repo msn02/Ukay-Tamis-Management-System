bf066e6a0fb134e5c95800621cf1d16b2d45b408
Misty Shaine Niones <125946850+msn02@users.noreply.github.com>
2024-05-14 17:38:48 +0800

    file: tables per category separated into files

---
+++ others/inventory_items.php
@@ -0,0 +1,43 @@
+<!-- single items -->
+<div class="row mx-0 mt-4 p-2 card border-0 rounded-2">
+    <table class="table shadow-md">
+        <thead class="">
+            <tr class="table_head">
+                <th scope="col-1">Image</th>
+                <th scope="col-1">Item ID</th>
+                <th scope="col-1">Name</th>
+                <th scope="col-1">Style</th>
+                <th scope="col-1">Size</th>
+                <th scope="col-1">Color</th>
+                <th scope="col-1">Unit in Stock</th>
+                <th scope="col-1">Price</th>
+                <th scope="col-2">Actions</th>
+            </tr>
+        </thead>
+        <tbody>
+            <!-- product details -->
+            <tr class="table_body align-middle text-center text_wrap">
+                <!-- product image -->
+                <td class="item_img">
+                    <img src="../resources/summer_fest2.png" class="rounded-2" alt="">
+                </td>
+                <td>1234</td>
+                <td class="lh-sm text-start" style="word-wrap: break-word;min-width: 160px;max-width: 160px;">Lorem ipsum dolor sit amet</td>
+                <td>Cottagecore</td>
+                <td>XXL</td>
+                <td>Nude</td>
+                <td>1</td>
+                <td class="fw-bold">PHP 250</td>
+                <!-- actions -->
+                <td class="text-center">
+                    <div class="m-0 p-0 hstack gap-2 d-flex justify-content-center gray_btn">
+                        <!-- edit order status -->
+                        <button id="edit_button" class="btn btn-secondary rounded-1"><i class="bi bi-pencil-square"></i></button>
+                        <!-- delete order -->
+                        <button class="btn btn-danger rounded-1"><i class="bi bi-trash3-fill"></i></button>
+                    </div>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</div>
---
+++ others/order_items.php
@@ -0,0 +1,50 @@
+<div class="row mx-0 mt-4 p-2 card border-0 rounded-2">
+    <table class="table shadow-md">
+        <thead class="">
+            <tr class="table_head">
+                <th scope="col">Transaction ID</th>
+                <th scope="col">User ID</th>
+                <th scope="col">Item ID</th>
+                <th scope="col">Item Name</th>
+                <th scope="col">Quantity</th>
+                <th scope="col">Shipping Fee</th>
+                <th scope="col">Total Price</th>
+                <th scope="col">Payment Method</th>
+                <th scope="col-1">Status</th>
+                <th scope="col">Actions</th>
+            </tr>
+        </thead>
+        <tbody>
+            <!-- order details -->
+            <tr class="table_body align-middle">
+                <td scope="row" class="text-start">1234</td>
+                <td class="text-start">1234</td>
+                <td class="text-center">1234</td>
+                <td class="text-center">Summer Fest</td>
+                <td class="text-center">1</td>
+                <td class="text-end">PHP 100</td>
+                <td class="text-end fw-bold">PHP 600</td>
+                <td class="text-center">GCash</td>
+                <!-- order status -->
+                <td class="text-center hstack gap-2 d-flex justify-content-center">
+                    <form class="form_option">
+                        <select id="status" class="form-select rounded-1" disabled>
+                            <option selected>Pending</option>
+                            <option value="1">Packed</option>
+                            <option value="2">Shipped</option>
+                        </select>
+                    </form>
+                </td>
+                <!-- actions -->
+                <td class="text-center">
+                    <div class="m-0 p-0 hstack gap-2 d-flex justify-content-center gray_btn">
+                        <!-- edit order status -->
+                        <button id="edit_button" class="btn btn-secondary rounded-1"><i class="bi bi-pencil-square"></i></button>
+                        <!-- delete order -->
+                        <button class="btn btn-danger rounded-1"><i class="bi bi-trash3-fill"></i></button>
+                    </div>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+</div>
