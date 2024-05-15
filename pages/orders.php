c820ca0b0a377c0521b1bb5ae2eca41b9b6d7268
Joseph Riosa <125950765+AlexHereToCode@users.noreply.github.com>
2024-05-15 22:00:41 +0800

    Update orders.php

--- pages/orders.php
+++ pages/orders.php
@@ -20,15 +20,16 @@
                     <input type="radio" class="btn-check rounded-1" name="record_option" id="items" autocomplete="off" value="order_items" checked>
                     <label class="btn btn-outline-dark px-3 py-2" for="items">SINGLE ITEMS</label>
 
-                    <input type="radio" class="btn-check rounded-1" name="record_option" id="box" autocomplete="off">
+                    <input type="radio" class="btn-check rounded-1" name="record_option" id="box" autocomplete="off" value="order_boxes">
                     <label class="btn btn-outline-dark px-3 py-2" for="box">BOXES</label>
                 </form>
             </div>
             <!-- searh bar -->   
             <div class="col-8 ms-auto search_input">
                 <form action="" class="green_btn hstack">
-                    <input class="form-control me-2 rounded-1 focus-ring-light focus-ring" type="search" placeholder="Search" aria-label="Search">
+                    <input class="form-control me-2 rounded-1 focus-ring-light focus-ring" id="searchInput" type="search" placeholder="Search by ID's" aria-label="Search">
                     <button class="btn btn-secondary border-0 rounded-1" type="submit">Search</button>
+                    
                 </form>
             </div>
         </div>
@@ -51,13 +52,44 @@
                     // show single items 
                     $('#orderItemsDiv').show();
                     $('#orderBoxesDiv').hide();
-                } else {
+                } else if ($(this).val() === 'order_boxes'){
                     // show mystery boxes
                     $('#orderItemsDiv').hide();
                     $('#orderBoxesDiv').show();
                 }
             });
         });
+
+  
+    // JavaScript for filtering table based on search input
+    document.getElementById("searchInput").addEventListener("input", function() {
+        var input, filter, table, tr, tdTransactionId, tdUserId, tdItemId, tdBoxId, i, txtValueTransactionId, txtValueUserId, txtValueItemId, txtValueBoxId;
+        input = this.value.toUpperCase();
+
+        // Determine which table is currently active
+        var activeTableId = document.querySelector('input[name="record_option"]:checked').value;
+        var activeTable = document.getElementById(activeTableId);
+
+        tr = activeTable.getElementsByTagName("tr");
+        for (i = 0; i < tr.length; i++) {
+            tdTransactionId = tr[i].getElementsByTagName("td")[0]; 
+            tdUserId = tr[i].getElementsByTagName("td")[1]; 
+            tdItemId = tr[i].getElementsByTagName("td")[2]; 
+            tdBoxId = tr[i].getElementsByTagName("td")[2]; 
+            if (tdTransactionId || tdUserId || tdItemId || tdBoxId) {
+                txtValueTransactionId = tdTransactionId.textContent || tdTransactionId.innerText;
+                txtValueUserId = tdUserId.textContent || tdUserId.innerText;
+                txtValueItemId = tdItemId.textContent || tdItemId.innerText;
+                txtValueBoxId = tdBoxId.textContent || tdBoxId.innerText;
+                if (txtValueTransactionId.toUpperCase().indexOf(input) > -1 || txtValueUserId.toUpperCase().indexOf(input) > -1 || txtValueItemId.toUpperCase().indexOf(input) > -1 || txtValueBoxId.toUpperCase().indexOf(input) > -1) {
+                    tr[i].style.display = "";
+                } else {
+                    tr[i].style.display = "none";
+                }
+            }
+        }
+    });
+
         </script>
     </div>
 </body>
